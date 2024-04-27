10d30f2a87dcb15feaec76746fc72a406f92b490
4004 <88835216+a4004@users.noreply.github.com>
2024-04-27 22:28:41 +0000

    Refactored urban.ts

--- src/modules/urban.ts
+++ src/modules/urban.ts
@@ -1,15 +1,21 @@
-import { EmbedBuilder } from "discord.js";
+/// urban.ts - Urban Dictionary module for orange🟠 Bot.
+/// Copyright © orangegrp 2024. All rights reserved.
+/// Refactored 27/04/2024.
+
 import type { Bot, Command } from "orange-bot-base";
+import { EmbedBuilder } from "discord.js";
 import { ArgType } from "orange-bot-base";
 
 const command = {
     name: "urban",
     description: "Get Urban dictionary definition for a word",
-    notes: "This command uses a cloud service. Data you provide may be shared with Urban Dictionary LLC (:flag_us:), subject to their [terms](https://about.urbandictionary.com/tos/) and [privacy policy](https://about.urbandictionary.com/privacy/).",
+    notes: "This command uses a cloud service. Data you provide may be shared with Urban Dictionary LLC (:flag_us:), " +
+        "subject to their [terms](https://about.urbandictionary.com/tos/) and [privacy policy](https://about.urbandictionary.com/privacy/).",
     args: {
         word: {
             type: ArgType.STRING,
-            description: "Word or phrase"
+            description: "Word or phrase",
+            required: true
         }
     }
 } as const satisfies Command;
@@ -30,46 +36,72 @@ type UrbanDefinition = {
     written_on?: string;
 }
 
-export default function(bot: Bot) {
+/**
+ * Get a list of possible definitioons from Urban Dictionary API.
+ * @param term Word or phrase to lookup (concated into the URL query with `encodeURIComponent`).
+ * @returns `UrbanDefinition[]` if the request was successful or `undefined` if something goes wrong.
+ */
+async function getDefinitions(term: string) {
+    const res = await fetch(`http://api.urbandictionary.com/v0/define?term=${encodeURIComponent(term)}`);
+    const body = await res.json() as UrbanResponse;
+
+    if (!body.list || !(body.list instanceof Array))
+        return undefined;
+    else
+        return body.list;
+}
+
+/**
+ * Get an embed with the **first** definition from an `UrbanDefinition[]`.
+ * @param definitions List of definitions from `getDefinitions` method.
+ * @returns A Discord `EmbedBuilder` object.
+ */
+async function getDefinition(definitions: UrbanDefinition[]) {
+    const definition = definitions[0];
+    definition.definition = definition.definition?.replace(/\[([\w\s]+)\]/g, (_, word: string) => `[${word}](http://${word.replace(/\s/g, "-")}.urbanup.com/)`);
+
+    const embed = new EmbedBuilder({
+        title: "Urban dictionary - " + definition.word,
+        url: definition.permalink,
+        description: definition.definition,
+        timestamp: definition.written_on,
+        author: definition.author ? {
+            name: definition.author,
+        } : undefined,
+        footer: { text: `Content from urbandictionary.com. The views/opinions expressed are that of their respective authors` },
+    });
+
+    if (definition.example !== undefined && definition.example.length > 0) {
+        embed.addFields({ name: "Example", value: definition.example.replace(/\[([\w\s]+)\]/g, (_, word: string) => `[${word}](http://${word.replace(/\s/g, "-")}.urbanup.com/)`) });
+    }
+
+    if (definition.thumbs_up && definition.thumbs_down) {
+        embed.addFields({ name: 'Rating', value: `:thumbsup: ${definition.thumbs_up} :thumbsdown: ${definition.thumbs_down}` });
+    }
+
+    return embed;
+}
+
+/**
+ * `urban.ts` - Urban Dictionary module for orange🟠 Bot.
+ * @param bot Bot object (`orange-bot-base`)
+ */
+export default function (bot: Bot) {
     bot.addCommand(command, async (interaction, args) => {
-        const res = await fetch(`http://api.urbandictionary.com/v0/define?term=${args.word}`);
+        const definitions = await getDefinitions(args.word);
 
-        const body = await res.json() as UrbanResponse;
-
-        if (!body.list || !(body.list instanceof Array)) throw new Error("Invalid response from urbandict api.");
-
-        if (!body.list[0]) {
-            interaction.reply({embeds: [{
-                title: `Urban dictionary - "${args.word}"`,
-                description: "No definition found for that term. Try another term, or check the spelling of the term you entered.",
-                timestamp: new Date().toISOString()
-            }]});
+        if (definitions === undefined || definitions.length === 0) {
+            interaction.reply({
+                embeds: [{
+                    title: `Urban dictionary - "${args.word}"`,
+                    description: "No definition found for that term. Try another term, or check the spelling of the term you entered.",
+                    timestamp: new Date().toISOString()
+                }]
+            });
             return;
         }
 
-        const definition = body.list[0];
-
-        definition.definition = definition.definition?.replace(/\[([\w\s]+)\]/g, (_, word: string) => `[${word}](http://${word.replace(/\s/g, "-")}.urbanup.com/)`);
-        
-        const embed = new EmbedBuilder({
-            title: "Urban dictionary - " + definition.word,
-            url: definition.permalink,
-            description: definition.definition,
-            timestamp: definition.written_on,
-            author: definition.author ? {
-                name: definition.author,
-            } : undefined,
-            footer: { text: `Content from urbandictionary.com. The views/opinions expressed are that of their respective authors` },
-        }); 
-
-        if (definition.example !== undefined && definition.example.length > 0) {
-            embed.addFields( { name: "Example", value: definition.example.replace(/\[([\w\s]+)\]/g, (_, word: string) => `[${word}](http://${word.replace(/\s/g, "-")}.urbanup.com/)`) } );
-        }
-
-        if (definition.thumbs_up && definition.thumbs_down) {
-            embed.addFields({ name: 'Rating', value: `:thumbsup: ${definition.thumbs_up} :thumbsdown: ${definition.thumbs_down}`} );
-        }
-
-        interaction.reply({embeds: [embed]});
+        const embed = await getDefinition(definitions);
+        interaction.reply({ embeds: [embed] });
     });
 }
