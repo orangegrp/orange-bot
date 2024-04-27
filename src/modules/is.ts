a2e1f1ed0e08f0ddfd9208d5478d021811018442
4004 <88835216+a4004@users.noreply.github.com>
2024-04-27 23:04:48 +0000

    refactored is.ts

--- src/modules/is.ts
+++ src/modules/is.ts
@@ -1,3 +1,7 @@
+/// is.ts - "is <question here>?" module for orange🟠 Bot.
+/// Copyright © orangegrp 2024. All rights reserved.
+/// Refactored 27/04/2024.
+
 import type { Bot, Command } from "orange-bot-base";
 import { ArgType } from "orange-bot-base";
 import sha256 from "sha256";
@@ -22,7 +26,27 @@ const responses = [
     "**Yes.**"
 ];
 
-export default function(bot: Bot) {
+/**
+ * Magic function to determine the answer.
+ * @param s Input string
+ * @returns One of the possible `responses` based on the input.
+ */
+function magic(s: string) {
+    // count number of "not" and "never" to see if the result should be inverted
+    const invert = s.split(" ").filter(word => word.match(/not|never/)).length % 2 == 1;
+    // remove the "not" and "never", sha256 the string
+    s = sha256(s.replace(/ ?(?:not|never) ?/g, ""));
+    for (var i = 0, h = 0; i < s.length; i++)
+        h = Math.imul(31, h) + s.charCodeAt(i) | 0;
+    const num = Math.round(Math.sqrt(Math.abs(h))) + (invert ? 2 : 0);
+    return responses[num % responses.length];
+}
+
+/**
+ * `is.ts` - "is <question here>?" module for orange🟠 Bot.
+ * @param bot Bot object (`orange-bot-base`)
+ */
+export default function (bot: Bot) {
     bot.addCommand(command, (interaction, args) => {
         const question = args.question
             .toLowerCase()         // lowercase
@@ -30,7 +54,7 @@ export default function(bot: Bot) {
             .replace(/ +/g, " ")   // remove duplicate spaces
             .replace(/`/g, "\u1fef")   // escape backtick
             .replace(/^is +/, ""); // make sure question doesn't start with "is" (duplicate is)
-        
+
         const does = question.startsWith("does");
         question.replace(/^does +/, "");
 
@@ -38,15 +62,4 @@ export default function(bot: Bot) {
 
         interaction.reply({ embeds: [{ description: response }], allowedMentions: { users: [], roles: [] } });
     })
-}
-
-function magic(s: string) {
-    // count number of "not" and "never" to see if the result should be inverted
-    const invert = s.split(" ").filter(word => word.match(/not|never/)).length % 2 == 1;
-    // remove the "not" and "never", sha256 the string
-    s = sha256(s.replace(/ ?(?:not|never) ?/g, ""));
-    for(var i = 0, h = 0; i < s.length; i++)
-        h = Math.imul(31, h) + s.charCodeAt(i) | 0;
-    const num = Math.round(Math.sqrt(Math.abs(h))) + (invert ? 2 : 0);
-    return responses[num % responses.length];
 }
