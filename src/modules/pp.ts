ac9de3f55cf5497e35f37c2c3eb3e1339ce66b33
4004 <88835216+a4004@users.noreply.github.com>
2024-04-27 23:01:09 +0000

    refactored pp.ts

--- src/modules/pp.ts
+++ src/modules/pp.ts
@@ -1,3 +1,8 @@
+/// pp.ts - pp module for orange🟠 Bot.
+/// Copyright © orangegrp 2024. All rights reserved.
+/// Refactored 27/04/2024.
+
+import type { Snowflake } from "discord.js";
 import type { Bot, Command } from "orange-bot-base";
 import { ArgType } from "orange-bot-base";
 
@@ -12,15 +17,15 @@ const command = {
     }
 } satisfies Command;
 
-export default function(bot: Bot) {
-    bot.commandManager.addCommand(command, (interaction, args) => {
-        bot.noPingReply(interaction, { content: pp(args.person?.id || interaction.user.id) });
-    });
-}
-
-function pp(id: string | undefined, min: number = 1, max: number = 50) {
+/**
+ * Generate pp
+ * @param id Snowflake identifier of the user.
+ * @param min Minimum size of the pp (default is `1`).
+ * @param max Maximum size of the pp (default is `50`).
+ * @returns The pp string.
+ */
+function pp(id: Snowflake, min: number = 1, max: number = 50) {
     const count = Math.floor(Math.random() * (max - min)) + min;
-
     let p = '8';
 
     for (let i = 0; i < count; i++)
@@ -32,4 +37,14 @@ function pp(id: string | undefined, min: number = 1, max: number = 50) {
         return `<@${id}>'s pp ${p}`;
 
     return p;
+}
+
+/**
+ * `pp.ts` - pp module for orange🟠 Bot.
+ * @param bot Bot object (`orange-bot-base`)
+ */
+export default function (bot: Bot) {
+    bot.commandManager.addCommand(command, (interaction, args) => {
+        bot.noPingReply(interaction, { content: pp(args.person?.id || interaction.user.id) });
+    });
 }
