4e5b16b3c9a560832fc6c17b75666ae51ccf0a0a
4004 <88835216+a4004@users.noreply.github.com>
2024-04-28 09:48:50 +0000

    refactored cveInfo.ts

--- src/modules/cve/cveInfo.ts
+++ src/modules/cve/cveInfo.ts
@@ -1,75 +1,69 @@
+/// cveInfo.ts - cve module for orange🟠 Bot.
+/// Copyright © orangegrp 2024. All rights reserved.
+/// Refactored 28/04/2024.
+
+/**
+ * OpenCVE.io API request wrapper.
+ * @param query_url Full API request URL.
+ * @returns `JSON` response, type `any` or undefined if error.
+ */
+async function openCveApiRequest(query_url: string) {
+    const response = await fetch(query_url, {
+        headers:
+        {
+            'Authorization': 'Basic ' + Buffer.from(process.env.OPENCVE_USER + ":" + process.env.OPENCVE_PASS).toString('base64'),
+            'Accept': 'application/json'
+        }
+    });
+
+    if (response.status === 404) {
+        return undefined;
+    } else if (!`${response.status}`.startsWith("20")) {
+        throw new Error(`Failed to retrieve data. Server returned code ${response.status}`);
+    }
+
+    return response.json();
+}
+
+/**
+ * CVE search method.
+ * @param args Object containing optional parameters: `keyword`, `vendor`, `product`, `cvss`, `cwe`
+ * @param page Page number to query.
+ * @returns Lookup result.
+ */
 async function getCvesData(args: { keyword?: string | null, vendor?: string | null, product?: string | null, cvss?: string | null, cwe?: string | null }, page: number = 1): Promise<OpenCveAPIResults | undefined> {
     let api_url = 'https://www.opencve.io/api/cve';
-    const append_query = (param: string, value: string) => api_url.endsWith("/cve") ? (api_url += `?${param}=${value}`) : (api_url += `&${param}=${value}`);
+    const append_query = (param: string, value: string) => api_url += `${(api_url.endsWith("/cve") ? '?' : "&")}${param}=${value}`;
 
-    if (args.keyword) {
-        append_query("search", args.keyword);
+    for (const [key, value] of Object.entries(args)) {
+        if (key && value) append_query(key, value);
     }
-    if (args.vendor) {
-        append_query("vendor", args.vendor);
-    }
-    if (args.product) {
-        append_query("product", args.product);
-    }
-    if (args.cvss) {
-        append_query("cvss", args.cvss);
-    }
-    if (args.cwe) {
-        append_query("cwe", args.cwe);
-    }
+
     if (page > 1) {
         append_query("page", page.toString());
     }
-
-    const response = await fetch(api_url, { headers: 
-        { 'Authorization': 'Basic ' + Buffer.from(process.env.OPENCVE_USER + ":" + process.env.OPENCVE_PASS).toString('base64'),
-          'Accept': 'application/json' }});
     
-    const res_json = await response.json();
-
-    if (response.status === 404 || res_json === undefined || res_json.length < 1) {
-        return undefined;
-    } else if (!`${response.status}`.startsWith("20")){
-        throw new Error(`Failed to retrieve data. Code: ${response.status}`);
-    }
-
-    return res_json as OpenCveAPIResults;
+    return await openCveApiRequest(api_url) as OpenCveAPIResults;
 }
 
+/**
+ * CVE ID lookup method.
+ * @param cveid Common vulnerabilites and exposures (CVE) identifier.
+ * @returns Lookup result.
+ */
 async function getCveInfoData(cveid: string): Promise<OpenCveAPIResultsInfo | undefined> {
     let api_url = 'https://www.opencve.io/api/cve/' + encodeURIComponent(cveid);
-
-    const response = await fetch(api_url, { headers: 
-        { 'Authorization': 'Basic ' + Buffer.from(process.env.OPENCVE_USER + ":" + process.env.OPENCVE_PASS).toString('base64'),
-          'Accept': 'application/json' }});
-
-    const res_json = await response.json();
-
-    if (response.status === 404 || res_json === undefined || res_json.length < 1) {
-        return undefined;
-    } else if (!`${response.status}`.startsWith("20")){
-        throw new Error(`Failed to retrieve data. Code: ${response.status}`);
-    }
-
-    return res_json as OpenCveAPIResultsInfo;
+    return await openCveApiRequest(api_url) as OpenCveAPIResultsInfo;
 }
 
+/**
+ * CWE ID lookup method.
+ * @param cweid Common Weakness and Exploitation (CWE) identifier.
+ * @returns Lookup result.
+ */
 async function getCweInfoData(cweid: string): Promise<OpenCweAPICweResultsInfo | undefined> {
     let api_url = 'https://www.opencve.io/api/cwe/' + encodeURIComponent(cweid);
-
-    const response = await fetch(api_url, { headers: 
-        { 'Authorization': 'Basic ' + Buffer.from(process.env.OPENCVE_USER + ":" + process.env.OPENCVE_PASS).toString('base64'),
-          'Accept': 'application/json' }});
-
-    const res_json = await response.json();
-
-    if (response.status === 404 || res_json === undefined || res_json.length < 1) {
-        return undefined;
-    } else if (!`${response.status}`.startsWith("20")){
-        throw new Error(`Failed to retrieve data. Code: ${response.status}`);
-    }
-
-    return res_json as OpenCweAPICweResultsInfo;
+    return await openCveApiRequest(api_url) as OpenCweAPICweResultsInfo;
 }
 
 export { getCvesData, getCveInfoData, getCweInfoData };
