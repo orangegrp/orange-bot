0172ff15f1a39fa71f7feb61526d8a8d38b3b75b
tpslr <topias.lyyra@gmail.com>
2024-07-20 22:16:57 +0300

    migrated assistant module to new config system

--- src/modules/gpt/costmgr.ts
+++ src/modules/gpt/costmgr.ts
@@ -1,6 +1,7 @@
 import pocketbase from "pocketbase";
 import { getLogger } from "orange-common-lib";
 import { pb, initDb } from "../../core/pocketbase.js";
+import { Bot, ConfigConfig, ConfigStorage, ConfigValueType } from "orange-bot-base";
 
 const logger = getLogger("costMgr");
 
@@ -19,100 +20,139 @@ type ora_user = {
 };
 
 
-async function getOraUser(user_id: string): Promise<ora_user | undefined> {
-    try {
-        const query = `user_id = '${user_id}'`;
-        const result = await pb.collection("ora_users").getFirstListItem<ora_user>(query);
-        return result;
-    } catch (err: any) {
-        logger.warn(`Failed to get user with snowflake ${user_id}`);
-        logger.error(err);
-        return undefined;
+const configConfig = {
+    name: "assistant",
+    displayName: "Assistant",
+    user: {
+        name: {
+            displayName: "Name",
+            description: "Your name",
+            type: ConfigValueType.string,
+        },
+        banned: {
+            displayName: "Banned",
+            description: "Are you banned?",
+            type: ConfigValueType.boolean,
+            default: false,
+            uiVisibility: "readonly"
+        },
+        dailyCost: {
+            displayName: "Daily Cost",
+            description: "",
+            type: ConfigValueType.number,
+            default: 0,
+            uiVisibility: "readonly"
+        },
+        dailyCostCap: {
+            displayName: "Daily Cost Cap",
+            description: "",
+            type: ConfigValueType.number,
+            default: Number(process.env.OPENAI_DAILY_COST_CAP),
+            uiVisibility: "readonly"
+        },
+        totalCost: {
+            displayName: "Total Cost",
+            description: "",
+            type: ConfigValueType.number,
+            default: 0,
+            uiVisibility: "readonly"
+        },
+        totalRequests: {
+            displayName: "Total Requests",
+            description: "",
+            type: ConfigValueType.number,
+            default: 0,
+            uiVisibility: "readonly"
+        },
+        totalTokens: {
+            displayName: "Total Tokens",
+            description: "",
+            type: ConfigValueType.number,
+            default: 0,
+            uiVisibility: "readonly"
+        },
+        creationDate: {
+            displayName: "Created",
+            description: "",
+            type: ConfigValueType.string,
+            uiVisibility: "readonly"
+        },
+        updateDate: {
+            displayName: "Updated",
+            description: "",
+            type: ConfigValueType.string,
+            uiVisibility: "readonly"
+        },
     }
-}
+} satisfies ConfigConfig
 
-async function resetAllDailyCaps() {
-    const users = await pb.collection("ora_users").getFullList<ora_user>();
-    for (const user of users) {
-        logger.log(`Resetting daily cost cap for user ${user.user_id}...`);
-        await updateOraUser(user.user_id, { daily_cost: 0 });
+
+class CostMgr {
+    readonly config;
+    constructor(bot: Bot) {
+        this.config = new ConfigStorage(configConfig, bot);
     }
-}
+    async getUser(user_id: string) {
+        return this.config.user(user_id).getAll();
+    }
+    async userExists(user_id: string) {
+        return !!await this.config.user(user_id).get("creationDate");
+    }
 
-async function createOraUser(user_id: string, name: string): Promise<string | undefined> {
-    try {
-        const user = await pb.collection("ora_users").create<ora_user>(
-            { 
-                user_id: user_id,
+    async resetAllDailyCaps() {
+        await this.config.setAllUsers("dailyCost", 0)
+    }
+
+    async createOraUser(user_id: string, name: string): Promise<string | undefined> {
+        try {
+            this.config.user(user_id).setMany({ 
+                creationDate: new Date().toISOString(),
                 name: name,
-                banned: false,
-                daily_cost: 0,
-                daily_cost_cap: Number(process.env.OPENAI_DAILY_COST_CAP),
-                total_cost: 0,
-                total_requests: 0,
-                total_tolens: 0
-            }
-            );
-        return user.id;
-    } catch (err: any) {
-        logger.warn(`Failed to create user with snowflake ${user_id}`);
-        logger.error(err);
-        return undefined;
-    }
-}
-
-async function updateOraUser(user_id: string, data: Partial<ora_user>): Promise<boolean> {
-    try {
-        const user = await getOraUser(user_id);
-        if (!user)
-            return false;
-
-        await pb.collection("ora_users").update<ora_user>(user.id, data);
-        return true;
-    } catch (err: any) {
-        logger.warn(`Failed to update user with snowflake ${user_id}`);
-        logger.error(err);
-        return false;
-    }
-}
-
-async function allowUser(user_id: string): Promise<boolean> {
-    logger.log(`Checking if user ${user_id} is allowed to use GPT (404 error may happen) ...`);
-
-    const user = await getOraUser(user_id);
-
-    if (user === undefined) {
-        return false;
-    }
-    if (user.banned) {
-        return false;
-    }
-    if (Date.now() - new Date(user.updated).getTime() >= 24 * 60 * 60 * 1000) {
-        if (await updateOraUser(user_id, { daily_cost: 0 })) {
-            return allowUser(user_id);
+            });
+            return user_id;
+        } catch (err: any) {
+            logger.warn(`Failed to create user with snowflake ${user_id}`);
+            logger.error(err);
+            return undefined;
         }
     }
-    if (user.daily_cost >= user.daily_cost_cap)
-        return false;
 
-    return true;
+    async allowUser(user_id: string): Promise<boolean> {
+        logger.log(`Checking if user ${user_id} is allowed to use GPT (404 error may happen) ...`);
+
+        const user = await this.config.user(user_id).getAll();
+
+        if (!user.creationDate) {
+            return false;
+        }
+        if (user.banned) {
+            return false;
+        }
+        if (Date.now() - new Date(user.updateDate || "0").getTime() >= 24 * 60 * 60 * 1000) {
+            await this.config.user(user_id).setMany({ updateDate: new Date().toISOString(), dailyCost: 0 });
+        }
+        if (user.dailyCost >= user.dailyCostCap)
+            return false;
+
+        return true;
+    }
+
+    calculateCost(sys_prompt_tokens: number, input_tokens: number, output_tokens: number): {
+        total_tokens: number,
+        input_cost: number,
+        output_cost: number,
+        total_cost: number
+    } {
+        const cost_per_1k_usd_input = Number(process.env.OPENAI_INPUT_COST!);
+        const cost_per_1k_usd_output = Number(process.env.OPENAI_OUTPUT_COST!);
+        const total_tokens = input_tokens + output_tokens;
+
+        const input_cost = ((input_tokens + sys_prompt_tokens) / 1000) * cost_per_1k_usd_input;
+        const output_cost = (output_tokens / 1000) * cost_per_1k_usd_output;
+        const total_cost = input_cost + output_cost;
+
+        return { total_tokens, input_cost, output_cost, total_cost };
+    }
 }
 
-function calculateCost(sys_prompt_tokens: number, input_tokens: number, output_tokens: number): {
-    total_tokens: number,
-    input_cost: number,
-    output_cost: number,
-    total_cost: number
-} {
-    const cost_per_1k_usd_input = Number(process.env.OPENAI_INPUT_COST!);
-    const cost_per_1k_usd_output = Number(process.env.OPENAI_OUTPUT_COST!);
-    const total_tokens = input_tokens + output_tokens;
-
-    const input_cost = ((input_tokens + sys_prompt_tokens) / 1000) * cost_per_1k_usd_input;
-    const output_cost = (output_tokens / 1000) * cost_per_1k_usd_output;
-    const total_cost = input_cost + output_cost;
-
-    return { total_tokens, input_cost, output_cost, total_cost };
-}
-
-export { allowUser, getOraUser, updateOraUser, createOraUser, calculateCost, ora_user, resetAllDailyCaps };
+export { CostMgr, ora_user };
