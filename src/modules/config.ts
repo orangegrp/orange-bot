0b0aa280b39810e950297089736c402179a001d6
tpslr <topias.lyyra@gmail.com>
2024-10-30 16:55:27 +0200

    added ?config get chat command

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -1,5 +1,7 @@
+import { Message } from "discord.js";
 import { Bot, ConfigValueType, Module } from "orange-bot-base";
-import { ConfigValues, ConfigValueScope } from "orange-bot-base/dist/ConfigStorage/types";
+import { ConfigStorage, ConfigurableI } from "orange-bot-base/dist/ConfigStorage/configStorage";
+import { ConfigConfig, ConfigValues, ConfigValueScope } from "orange-bot-base/dist/ConfigStorage/types";
 
 const FORMAT_LIST = "`?config list <module>`";
 const FORMAT_GET = "`?config get <module>.<user|guild|global>.<valuename>`";
@@ -12,7 +14,7 @@ const USAGE_SET = `Usage: ${FORMAT_SET}`;
 const USAGE_ALL = `Usage:\n${FORMAT_LIST}\n${FORMAT_GET}\n${FORMAT_SET}`;
 
 export default function (bot: Bot, module: Module) {
-    bot.addChatCommand("config", (msg, args) => {
+    bot.addChatCommand("config", async (msg, args) => {
         const allPerms = msg.author.id === "239170246118735873"  // alex
                       || msg.author.id === "321921856611418125"  // topias
                       || msg.author.id === "912484519301500948"; // persist
@@ -35,7 +37,7 @@ export default function (bot: Bot, module: Module) {
             }
 
             const data = parseValueName(args[1])
-            //msg.reply(getValue(data));
+            msg.reply(await getValue(data, msg));
         }
         else if (action === "set") {
             if (!args[1] || !args[2]) {
@@ -64,26 +66,62 @@ export default function (bot: Bot, module: Module) {
         
         return out;
     }
-    function getValue(value: ValueData) {
+    async function getValue(value: ValueData, message: Message) {
+        if (!message.inGuild()) {
+            return `This can only be used in guilds`;
+        }
+        const storage = bot.configApi.storages.get(value.module);
+        if (!storage) return `There's no storage for module ${value.module}!`;
 
+        const [exists, err] = checkValueExists(value, storage);
+        if (!exists)
+            return err;
+
+        const configurable = (value.scope === "user"   ? storage.user(message.author)
+                            : value.scope === "guild"  ? storage.guild(message.guild)
+                            : storage.global()) as ConfigurableI<ConfigConfig, ConfigValueScope>;
+
+        const stringValue = JSON.stringify(await configurable.get(value.name), null, 4);
+
+        return `${value.module}.${value.scope}.${value.name} = ${stringValue}`;
     }
-    function setValue(data: ValueData, value: string) {
+    function setValue(data: ValueData, value: string, message: Message) {
+    }
 
+    function checkValueExists(value: ValueData, storage: ConfigStorage<ConfigConfig>): [true, undefined] | [false, string] {
+        if (!isValidScope(value.scope)) {
+            return [false, `"${value.scope}" is not a valid config scope. Options: "user", "guild", "global"`];
+        }
+
+        const schema = storage.config[value.scope];
+        if (!schema) {
+            return [false, `"${value.module}" doesn't have any values in scope "${value.scope}"!`];
+        }
+
+        if (!schema[value.name]) {
+            return [false, `"${value.module}.${value.scope}" does not include a value called "${value.name}"`];
+        }
+        return [true, undefined];
     }
 }
 
 type ValueData = {
     module: string;
     scope: string;
-    value: string[];
+    name: string;
+    path: string[];
 }
 
 function parseValueName(valueName: string): ValueData {
-    const [module, scope, ...value] = valueName.split(".");
+    const [module, scope, name, ...path] = valueName.split(".");
     
     return {
-        module, scope, value
+        module, scope, name, path
     }
+}
+
+function isValidScope(scope: string): scope is ConfigValueScope {
+    return ["user", "guild", "global"].includes(scope);
 }
 
 function listOptionsFromSchema(module: string, scope: ConfigValueScope, schema: ConfigValues<ConfigValueScope>, showAll: boolean) {
