7688c26b678468d276134b7d1d20b4be23b97685
tpslr <topias.lyyra@gmail.com>
2024-11-22 14:35:52 +0200

    made /russian timeout duration configurable

--- src/modules/russian.ts
+++ src/modules/russian.ts
@@ -2,7 +2,7 @@
 /// Copyright © orangegrp 2024. All rights reserved.
 /// Refactored 27/04/2024.
 
-import { AttachmentBuilder, GuildMemberRoleManager, Role } from "discord.js";
+import { AttachmentBuilder } from "discord.js";
 import { ConfigStorage, ConfigValueType, type Bot, type Command, type ConfigConfig, type Module } from "orange-bot-base";
 
 const roulette = new AttachmentBuilder("src/modules/russian/roulette.gif", { name: 'roulette.gif' });
@@ -15,6 +15,20 @@ const command = {
     args: {}
 } satisfies Command;
 
+const configSchema = {
+    name: "russian",
+    displayName: "Russian roulette",
+    guild: {
+        timeoutDuration: {
+            displayName: "Timeout duration (minutes)",
+            description: "Duration of timeout applied by russian roulette",
+            permissions: "ModerateMembers",
+            type: ConfigValueType.number,
+            default: 5,
+        }
+    }
+} satisfies ConfigConfig;
+
 /**
  * Generate russian
  * @returns Mute notification
@@ -26,7 +40,10 @@ const russian = () => { return Math.random() < 0.17; };
  * `pp.ts` - pp module for orange🟠 Bot.
  * @param bot Bot object (`orange-bot-base`)
  */
-export default function (bot: Bot, module: Module) {
+export default async function (bot: Bot, module: Module) {
+    const config = new ConfigStorage(configSchema, bot);
+    await config.waitForReady();
+
     module.addCommand(command, async (interaction, args) => {
         if (!interaction.inGuild()) {
             await bot.replyWithError(interaction, "This can't be used outside a server. :(");
@@ -42,7 +59,7 @@ export default function (bot: Bot, module: Module) {
             ]
         });
 
-        // Wait for 3 seconds
+        // Wait for 5 seconds
         setTimeout(async () => {
             const result = russian();
             await interaction.editReply({ 
@@ -55,8 +72,9 @@ export default function (bot: Bot, module: Module) {
                     ]
                 });
 
-            // If the user lost, mute them for 5 minutes
+            // If the user lost, mute them for x minutes
             if (result) {
+                const timeoutDuration = await config.guild(interaction.guildId).get("timeoutDuration");
 
                 const member = "timeout" in interaction.member
                     ? interaction.member
@@ -73,10 +91,10 @@ export default function (bot: Bot, module: Module) {
                     });
                     return;
                 }
-                await member.timeout(5 * 60 * 1000);
+                await member.timeout(timeoutDuration * 60 * 1000);
                 
                 await interaction.followUp({
-                    content: `You have been timed out for 5 minutes.`,
+                    content: `You have been timed out for ${timeoutDuration} minutes.`,
                     ephemeral: true
                 });
             }
